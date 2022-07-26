ce7376dfa94bca1aba3cc766ca5bb16b9e541fd7
Gabriel <gabriel.ballesteros@colimasoft.com>
2022-07-26 18:01:48 -0500

    working on the user skills update

--- auth0-test/pages/getlinkedindata.js
+++ auth0-test/pages/getlinkedindata.js
@@ -1,22 +1,67 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import React, { useState } from 'react'
+import cheerio from 'cheerio'
+import axios from 'axios'
+
+// export const getServerSideProps = async (context) => {
+
+//   // const { user } = useUser()
+
+//   const { html } = await axios.get('https://www.theguardian.com/uk')
+//   const $ = cheerio.load(html)
+
+//   const title = $('.fc-it-em__title').first().text()
+
+//   return {
+//     props: { title }
+// W,O[= REQdaszxc  }
+// }
 
 
 const examplepage = () => {
 
-  const { user } = useUser()
+  const html = async () => {
+    let data
+    return data = await axios.get('https://www.theguardian.com/uk')
+  }
 
+  console.log(html())
 
   return (
-    user ? (
-      <div>
-        authorized
-        <p>{user.email}</p>
-      </div >
-    ) : (
-      <p>cant access this page</p>
-    )
+    <div>
+      {/* <h1>{title}</h1> */}
+    </div>
   )
 }
 
 export default examplepage
+
+
+// const url = 'https://www.theguardian.com/uk'
+
+// app.get('/', function (req, res) {
+//     res.json('This is my webscraper')
+// })
+
+// app.get('/results', (req, res) => {
+//     axios(url)
+//         .then(response => {
+//             const html = response.data
+//             const $ = cheerio.load(html)
+//             const articles = []
+
+//             $('.fc-item__title', html).each(function () { //<-- cannot be a function expression
+//                 const title = $(this).text()
+//                 const url = $(this).find('a').attr('href')
+//                 articles.push({
+//                     title,
+//                     url
+//                 })
+//             })
+//             res.json(articles)
+//         }).catch(err => console.log(err))
+
+// })
+
+
+// app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
